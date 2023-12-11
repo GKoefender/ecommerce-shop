02da98c2ba396ca9f6116a2823d8b181445db097
gkoefender <gustavokoefender@gmail.com>
2023-12-11 13:29:19 -0300

    header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app'
 import globalStyle from './styles/globalStyle' 
+import logo from './assets/Logo.svg'
+import { Container, Header } from './styles/pages/app'
 
 globalStyle()
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <img src={logo.src} alt="Logo" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
---
+++ src/pages/styles/pages/app.ts
@@ -0,0 +1,18 @@
+import { styled } from '../index'
+
+const Container = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  // justifyContent: 'center',
+  height: '100vh'
+})
+
+const Header = styled('header', {
+  padding: '2rem 0',
+  width: '100%',
+  maxWidth: 1180,
+  margin: '0 auto'
+})
+
+export { Container, Header }
