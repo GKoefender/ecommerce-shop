2941809b7368774b798f6206bd1db5aa719bf6d7
gkoefender <gustavokoefender@gmail.com>
2023-12-12 11:04:04 -0300

    image tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+
 import globalStyle from './styles/globalStyle' 
 import logo from './assets/Logo.svg'
 import { Container, Header } from './styles/pages/app'
@@ -9,7 +11,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logo.src} alt="Logo" />
+        <Image src={logo} alt="Logo" />
       </Header>
 
       <Component {...pageProps} />
