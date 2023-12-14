d4d06a55310314f5366ada66f318fb39490a5482
gkoefender <gustavokoefender@gmail.com>
2023-12-14 08:49:34 -0300

    carousel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import { HomeContainer, Product } from '../styles/pages/home'
 import Image from 'next/image'
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
 
 import camisa1 from '../assets/camisetas/1.png'
 import camisa2 from '../assets/camisetas/2.png'
 
 export default function Home () {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef}>
+      <Product className="keen-slider__slide number-slide">
         <Image src={camisa1} alt="" width={520} height={480} />
 
         <footer>
@@ -16,7 +25,7 @@ export default function Home () {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide number-slide">
         <Image src={camisa2} alt="" width={520} height={480} />
 
         <footer>
@@ -24,6 +33,35 @@ export default function Home () {
           <span>R$ 79,90</span>
         </footer>
       </Product>
+
+      <Product className="keen-slider__slide number-slide">
+        <Image src={camisa2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide number-slide">
+        <Image src={camisa2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide number-slide">
+        <Image src={camisa2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camisa X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      
     </HomeContainer>
   )
 }
